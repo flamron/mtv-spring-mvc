5be16243f0a4c6fc5e03f1db22138546be6458fa
Renat Serahlazau <renat.seroglazov@gmail.com>
2019-09-13 16:14:51 +0300

    EOF

---
+++ src/main/java/by/pvt/springweb/controller/GreetingController.java
@@ -0,0 +1,7 @@
+package by.pvt.springweb.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GreetingController {
+}
---
+++ src/main/java/by/pvt/springweb/initializer/WebInitializer.java
@@ -0,0 +1,23 @@
+package by.pvt.springweb.initializer;
+
+import by.pvt.springweb.config.WebConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    private static final String SERVLET_MAPPING = "/";
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {SERVLET_MAPPING};
+    }
+}
